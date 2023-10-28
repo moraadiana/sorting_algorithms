5dcfd61ef9d88dd1b02d69896db7fd488f330df9
moraadiana13 <moraadiana13@gmail.com>
2023-10-28 04:02:49 -0700

    u

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -12,12 +12,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 
 	if (nmemb == 0 || size == 0)
-		return ('\0');
+		return NULL;
 	p = malloc(nmemb * size);
-	if (p == '\0')
-		return ('\0');
+	if (p == NULL)
+		return NULL;
 	for (i = 0; i < (nmemb * size); i++)
-		p[i] = '\0';
+		p[i] = NULL;
 	return (p);
 }
 /**
